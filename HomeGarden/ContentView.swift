b2a8ee50a087810e5b8f009ccc29f345f411f714
xiaosenai8 <xiaosenai8@gmail.com>
2025-10-05 16:44:53 +0900

    Initial Commit

---
+++ HomeGarden/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  HomeGarden
+//  
+//  Created by konishi on 2025/10/05
+//  
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HomeGarden/HomeGardenApp.swift
@@ -0,0 +1,18 @@
+//
+//  HomeGardenApp.swift
+//  HomeGarden
+//  
+//  Created by konishi on 2025/10/05
+//  
+//
+
+import SwiftUI
+
+@main
+struct HomeGardenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
