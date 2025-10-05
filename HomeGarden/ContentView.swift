0bd694c1a53bed16b7060e6a7f0e3d529caf732b
xiaosenai8 <xiaosenai8@gmail.com>
2025-10-05 17:01:16 +0900

    Start dev

--- HomeGarden/ContentView.swift
+++ HomeGarden/ContentView.swift
@@ -14,7 +14,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, HomeGarden!")
+            Text("Start dev HomeGarden!")
         }
         .padding()
     }
